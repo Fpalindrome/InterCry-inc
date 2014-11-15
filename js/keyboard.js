81f6f8f2775a7f0e049c117a01f4e75005bf7a79
mrBatata <mmarques.eped@gmail.com>
2014-11-15 20:51:46 +0000

    Primeiro deploy com index trocado por home

---
+++ js/keyboard.js
@@ -0,0 +1,58 @@
+/**
+ * @author ic18
+ */
+$(function(){
+    var $write = $('#write'),
+        shift = false,
+        capslock = false;
+     
+    $('#keyboard li').click(function(){
+        var $this = $(this),
+            character = $this.html(); //Se for uma letra minuscula, não acontece nada
+         
+        // Shift keys
+        if ($this.hasClass('left-shift') || $this.hasClass('right-shift')) {
+            $('.letter').toggleClass('uppercase');
+            $('.symbol span').toggle();
+             
+            shift = (shift === true) ? false : true;
+            capslock = false;
+            return false;
+        }
+         
+        // Caps lock
+        if ($this.hasClass('capslock')) {
+            $('.letter').toggleClass('uppercase');
+            capslock = true;
+            return false;
+        }
+         
+        // Delete
+        if ($this.hasClass('delete')) {
+            var html = $write.html();
+             
+            $write.html(html.substr(0, html.length - 1));
+            return false;
+        }
+         
+        // Caracteres espaciais 
+        if ($this.hasClass('symbol')) character = $('span:visible', $this).html();
+        if ($this.hasClass('space')) character = ' ';
+        if ($this.hasClass('tab')) character = "\t";
+        if ($this.hasClass('return')) character = "\n";
+         
+        // Maiusculas
+        if ($this.hasClass('uppercase')) character = character.toUpperCase();
+         
+        // Desactivar Shift assim que uma tecla for espremida
+        if (shift === true) {
+            $('.symbol span').toggle();
+            if (capslock === false) $('.letter').toggleClass('uppercase');
+             
+            shift = false;
+        }
+         
+        // Add the character
+        $write.html($write.html() + character);
+    });
+});
